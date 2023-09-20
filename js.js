28695e8b4d15842157783935df5df5a54befc0d4
mrohith29 <mariyalarohith29@gmail.com>
2023-09-20 14:23:02 +0530

    updated..

--- js.js
+++ js.js
@@ -3,3 +3,8 @@ console.log('hello world\njwb');
 a = 1;
 b = 2;
 console.log(a + b);
+console.warn('error occured')
+console.clear();
+console.log('hello' + ' ' + 'world');
+
+console.log(Math.random())
