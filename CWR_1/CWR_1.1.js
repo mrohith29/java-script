a0e30d07a48b349613514b9e43d8dffddd89c74a
mrohith29 <mariyalarohith29@gmail.com>
2023-11-27 16:26:57 +0530

    Add JavaScript introduction and basic understanding

---
+++ CWR_1/CWR_1.1.js
@@ -0,0 +1,31 @@
+/*
+Introduction To JavaScript
+-> Understand the basics of JavaScript
+-> Its role in web development
+-> Its essential syntax.
+*/
+
+
+// JavaScript is a scripting language that adds interactivity to the web pages.
+// It is a high level interpreted language, used in conjunction with HTML and CSS to create dynamic and engaging web pages.
+// JavaScript is also used in a variety of other contexts, including server-side development, game development, and mobile app development.
+
+/*
+Basic Understanding of JavaScript
+
+JavaScript is a versatile language that can be used to accomplish a wide range of tasks. Some of the things that JavaScript can be used for include:
+
+Adding animations and other dynamic effects to web pages
+Creating interactive forms and user interfaces
+Handling user input and events
+Making asynchronous requests to servers
+Accessing and manipulating the Document Object Model (DOM)
+Building games and other interactive applications
+JavaScript's Role in Web Development
+
+JavaScript is an essential part of modern web development.
+It is used by all major web browsers and is supported by a wide range of development tools and frameworks.
+JavaScript is used to create the interactive elements that make web pages more engaging and user-friendly.
+It is also used to develop complex web applications, such as single-page applications (SPAs) and progressive web apps (PWAs).
+*/
+
