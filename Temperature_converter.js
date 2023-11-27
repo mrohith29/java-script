8048175b57b56aa56c791b65ef0fc18444e75503
mrohith29 <mariyalarohith29@gmail.com>
2023-11-27 13:12:46 +0530

    made git repo

--- Temperature_converter.js
+++ Temperature_converter.js
@@ -3,3 +3,4 @@ const fahrenheit = celsius * (9/5) + 32;
 console.log(fahrenheit);
 // this is how we can do conversion from celsius to fahrenheit
 
+//new commernt line
--- js.js
+++
@@ -1,5 +0,0 @@
-let name = 'mariyala rohith'
-console.log(`my name is ${name}`)
-let age = 18
-age+=1
-console.log(`${name} age is ${age}`)
