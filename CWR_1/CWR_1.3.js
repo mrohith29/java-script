a37b458dfcb87281c0fa1d792f6bf705b377636b
mrohith29 <mariyalarohith29@gmail.com>
2023-11-28 12:24:05 +0530

    Add arithmetic, comparison, logical, and bitwise operators

---
+++ CWR_1/CWR_1.3.js
@@ -0,0 +1,34 @@
+// Operators
+
+// Arithmetic operator
+
+console.log(2 + 3); // addition
+console.log(2 - 3); // subtraction
+console.log(2 * 3); // multiplication
+console.log(10 / 5); // division (quotient as output)
+console.log(10 % 4); //division (remainder as output)
+console.log(2 ** 3); // exponential
+
+// Comparison operator
+
+console.log(1 == 2); // Equal to
+console.log((1 != 2)); // Not Equal to
+console.log( 1 > 2); // Greater than
+console.log(1 >= 1); // greater than or equal to
+console.log(1 < 2); // less than
+console.log(1 <= 0); // less than or equal to
+
+// Logical operators
+
+console.log((5 > 3) && (10 != 4)); // Output: true
+console.log((2 > 7) || (16 >= 4)); // Output: true
+console.log(! (11 < 3)); // Output: true
+
+// Bitwise operators
+
+console.log(5 & 3); // Bitwise and
+console.log(10 | 4); // Bitwise or
+console.log(~2); // ones compliment
+console.log(11 ^ 3); // Output: 10
+console.log(2 << 3); // Output: 16
+console.log(16 >> 4); // Output: 2
