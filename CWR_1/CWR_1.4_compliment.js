a497c182c51e19dfac4107294061da2359c9c2be
mrohith29 <mariyalarohith29@gmail.com>
2023-11-28 13:30:55 +0530

    added user inputting in js

---
+++ CWR_1/CWR_1.4_compliment.js
@@ -0,0 +1,5 @@
+// user input in java script
+const prompt = require('prompt-sync')();
+const name = prompt();
+const roll = prompt();
+console.log("The name of the student with roll number " + roll + " is " + name);
