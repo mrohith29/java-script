a923cd591842d68dec267df6b40c4d83498b1201
mrohith29 <mariyalarohith29@gmail.com>
2023-11-29 17:02:22 +0530

    updated...

--- practise file set.js
+++ practise file set.js
@@ -1,27 +1,31 @@
+// // let input = require('prompt-sync')()
+// // // let m = input("enter your name : ")
+// // // console.log("hello " + m)
+//
+// // let str = input("Enter the string to convert it into uppercase : ")
+// // console.log(str.toUpperCase())
+//
+//
+// function isprime(num) {
+//     let count = 0
+//     for(i = 1; i <= num; i++) {
+//         if(num % i == 0) {
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+//
 // let input = require('prompt-sync')()
-// // let m = input("enter your name : ")
-// // console.log("hello " + m)
+// let n = input('Enter the number to check the prime condition: ')
+// let x = isprime(n)
+// if(x == 2)
+// {
+//     console.log(n + " is a prime number")
+// } else {
+//     console.log(n + " is not a prime number")
+// }
 
-// let str = input("Enter the string to convert it into uppercase : ")
-// console.log(str.toUpperCase())
 
-
-function isprime(num) {
-    let count = 0
-    for(i = 1; i <= num; i++) {
-        if(num % i == 0) {
-            count++;
-        }
-    }   
-    return count;
-}
-
-let input = require('prompt-sync')()
-let n = input('Enter the number to check the prime condition: ')
-let x = isprime(n)
-if(x == 2)
-{
-    console.log(n + " is a prime number")
-} else {
-    console.log(n + " is not a prime number")
-}
+let str = "hello, hello, world"
+console.log(str.lastIndexOf("hello")) // output = 7
